b2608ab999bca94a5b4a8aef9b70a3c3a46b6c91
Lucca Lopes <110003708+luccalopes@users.noreply.github.com>
2023-09-02 16:16:02 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+function yes() {
+    declaration()
+}
+
+function desvia() {
+    const btn = document.querySelector('#desvia')
+    btn.style.position = 'absolute'
+    btn.style.bottom = toAlter(10, 90)
+    btn.style.left = toAlter(10, 90)
+}
+
+function toAlter(min, max) {
+    return `${Math.random() * (max - min) + min}%`
+}
+
+const btnYes = document.querySelector('#yes').addEventListener('click', yes)
+const btnNo = document.querySelector('#desvia').addEventListener('mouseover', desvia)
+
+function declaration() {
+    const declaration = document.getElementById('declaration')
+    declaration.className = 'declaration'
+    declaration.innerText = 'Bianca, Eu sou LOUCO por você! Desde que te conheci você não saiu mais da minha cabeça, quando penso em você eu sinto vontade de ser melhor, eu amo a sua presença e contigo eu me sinto capaz de ter o mundo! Você é especial, você é PERFEITA pra mim, meu amor!'
+    const main = document.getElementById('main')
+    main.innerText = ''
+    return declaration
+}
